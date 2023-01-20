efd61cd323ce828ca2d9cdeb522ba06e36d91e01
Sof <sofie151091@gmail.com>
2023-01-19 21:36:16 -0600

    added with the strictQuery set to false in line 7 in bd.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,6 +4,7 @@ const config = require("./config");
 const connect = () => {
   return new Promise(async (resolve, reject) => {
     const { user, password, host } = config.db;
+    mongoose.set("strictQuery", false);
 
     mongoose.connect(
       `mongodb+srv://${user}:${password}@${host}/?retryWrites=true&w=majority`
