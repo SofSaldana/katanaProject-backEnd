dd3665bf31d4a6786fb4e25d8606282574a3870b
panchoman32 <estebandido32@hotmail.com>
2023-01-19 19:44:43 -0700

    fixing project structure with users/products

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -1,13 +1,9 @@
-const cartRouter = require("./cartRouter");
-const ordersRouter = require("./ordersRouter");
 const productsRouter = require("./productsRouter");
-const userAdministratorRouter = require("./userAdministratorRouter");
+const usersRouter = require("./usersRouter");
 
 const router = (app) => {
-  app.use("/cart", cartRouter);
-  app.use("/orders", ordersRouter);
   app.use("/products", productsRouter);
-  app.use("/user", userAdministratorRouter);
+  app.use("/user", usersRouter);
 };
 
 module.exports = router;
