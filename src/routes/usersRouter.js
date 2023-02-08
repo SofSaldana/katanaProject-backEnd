2dff94bf998258056f7f76f1eb20aad4c3f8f5b4
panchoman32 <estebandido32@hotmail.com>
2023-02-08 15:07:29 -0700

    almost complete CRUD for users

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -15,4 +15,59 @@ router.post("/create", async (req, res) => {
   }
 });
 
+router.patch("/patch/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const update = req.body;
+    const patchUser = await user.updateUser(id, update);
+    if (!patchUser) throw new Error("User was not updated");
+    res.status(202).json({
+      message: "Updated",
+      payload: patchUser,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.get("/get", async (req, res) => {
+  try {
+    const showAllUsers = await user.getAllUsers();
+    res.status(200).json({
+      message: "Users",
+      payload: showAllUsers,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.get("/get/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const showUser = await user.getUser(id);
+    if (!showUser) throw new Error("User was not found");
+    res.status(302).json({
+      message: "User found",
+      payload: showUser,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const eraseUser = await user.deleteUser(id);
+    if (!eraseUser) throw new Error("User was not deleted");
+    res.status(202).json({
+      message: "User deleted",
+      payload: eraseUser.id,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
 module.exports = router;
--- src/usecases/users/index.js
+++ src/usecases/users/index.js
@@ -11,6 +11,29 @@ const newUser = async (userData) => {
   };
 };
 
+const updateUser = async (id, update) => {
+  const patchUser = await User.findByIdAndUpdate(id, update, {
+    new: true,
+  }).exec();
+  return patchUser;
+};
+
+const getAllUsers = async () => {
+  const findAllUsers = await User.find({}).exec();
+  return findAllUsers;
+};
+
+const getUser = async (id) => {
+  const findUser = await User.findById({ id }).exec();
+  return findUser;
+};
+
+const deleteUser = async (id) => await User.findByIdAndDelete(id).exec();
+
 module.exports = {
-    newUser
+  newUser,
+  updateUser,
+  deleteUser,
+  getAllUsers,
+  getUser,
 };
