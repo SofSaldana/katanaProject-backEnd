90040b54ee803aace347d836dad7432605101fe5
panchoman32 <estebandido32@hotmail.com>
2023-02-08 17:19:04 -0700

    change path to create users from 'create' to ' post'

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const user = require("../usecases/users");
 
-router.post("/create", async (req, res) => {
+router.post("/post", async (req, res) => {
   try {
     const newUser = await user.newUser(req.body);
     if (!newUser) throw new Error("User was not created");
