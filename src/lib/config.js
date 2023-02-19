ab2261c45cfab1aee53a4159f3ae1a2ce78f34c2
Alejandro Martinez <diegoalejandro.mx@gmail.com>
2023-02-19 15:36:46 -0600

    small changes

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,7 +1,14 @@
 require("dotenv").config();
 
-const { APP_PORT, APP_DB_HOST, APP_DB_PASSWORD, APP_DB_USER, APP_SECRET, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } =
-  process.env;
+const {
+  APP_PORT,
+  APP_DB_HOST,
+  APP_DB_PASSWORD,
+  APP_DB_USER,
+  APP_SECRET,
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+} = process.env;
 
 const config = {
   app: {
@@ -13,12 +20,10 @@ const config = {
     password: APP_DB_PASSWORD,
     host: APP_DB_HOST,
   },
-
   google: {
     clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET
-  }
-
+    clientSecret: GOOGLE_CLIENT_SECRET,
+  },
 };
 
 module.exports = config;
