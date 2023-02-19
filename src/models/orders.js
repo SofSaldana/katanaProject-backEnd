fd4960677ef717852cb9faacdb63e4c135a75be2
Alejandro Martinez <diegoalejandro.mx@gmail.com>
2023-02-19 15:32:44 -0600

    save spot before more changes

--- src/models/orders.js
+++ src/models/orders.js
@@ -5,3 +5,5 @@ const schema = new Schema({
   items: [{ type: Schema.Types.ObjectId, ref: "products" }],
   buyer: [{ type: Schema.Types.ObjectId, ref: "users" }],
 });
+
+module.exports = mongoose.model("orders", schema);
--- src/usecases/payment/index.js
+++ src/usecases/payment/index.js
@@ -1,10 +1,13 @@
 const mercadopago = require("mercadopago");
 const config = require("../../lib/config");
 
-mercadopago.configurations.setAccessToken(config.mercadopago.accessToken);
-
 const newOrder = async (orderData) => {
+  mercadopago.configurations.setAccessToken(config.mercadopago.accessToken);
   const order = new Order(orderData);
   const saveOrder = await order.save();
   return;
+};
+
+module.exports = {
+  newOrder,
 };
