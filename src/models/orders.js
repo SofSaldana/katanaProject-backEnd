afcf24733c2764c46ffc2b8489eb4bfdf9fd0a7e
Alejandro Martinez <diegoalejandro.mx@gmail.com>
2023-02-21 21:10:49 -0600

    orders model for mercadopagoapi

--- src/models/orders.js
+++ src/models/orders.js
@@ -1,9 +1,32 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const schema = new Schema({
-  items: [{ type: Schema.Types.ObjectId, ref: "products" }],
-  buyer: [{ type: Schema.Types.ObjectId, ref: "users" }],
-});
+const schema = new Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    items: [
+      {
+        ref: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        title: { type: String, required: true },
+        description: { type: String, required: true },
+        picture_url: { type: String, required: true },
+        quantity: { type: Number, required: true },
+        currency: { type: String },
+        totalPrice: { type: Number, required: true },
+      },
+    ],
+    isPaid: { type: Boolean, required: true, default: false },
+    paymentMethod: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("orders", schema);
