54a015b256cadfe90bfa3c40dd37734cf48f8e40
Alejandro Martinez <diegoalejandro.mx@gmail.com>
2023-02-23 21:04:16 -0600

    cleaning code for console log and const that we dont need

--- src/routes/paymentsCheckout.js
+++ src/routes/paymentsCheckout.js
@@ -1,18 +1,11 @@
-const config = require("../lib/config");
-const mercadopago = require("mercadopago");
 const { Router } = require("express");
 const router = Router();
 const payments = require("../usecases/payment/");
 
 router.post("/", async (req, res) => {
-  const estufas = req.body;
-  const preference = await payments.createPreference(estufas);
+  const preferenceBody = req.body;
+  const preference = await payments.createPreference(preferenceBody);
   res.status(200).json({ payload: preference });
 });
-
-// const { process, feedback } = require("../middlewares/mercadopago");
-// const products = require("../models/products");
-// router.post("/process", process);
-// router.get("/feedback", feedback);
 
 module.exports = router;
