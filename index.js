739fe3b568d8b7e8883b2e2845f2a3043452e88c
Alejandro Martinez <diegoalejandro.mx@gmail.com>
2023-02-25 16:44:41 -0600

    adding cors to pass petitions for all

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ router(app);
 
 app.use(cors());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.get("/", function (req, res, next) {
+  res.send("Runing");
+});
+
 app.listen(config.app.port, async () => {
   console.log(`Esuchando peticiones HTTP en el puerto ${config.app.port}`);
 
