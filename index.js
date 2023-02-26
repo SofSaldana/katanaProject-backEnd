9478c639a3f972193e801e3f93087a1c026bc392
Alejandro Martinez <diegoalejandro.mx@gmail.com>
2023-02-25 18:30:29 -0600

    commeting for experiment cors rule

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ app.use(cors());
 app.use(express.json());
 router(app);
 
-app.use(function (req, res, next) {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://blu-front-end.vercel.app/"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "https://blu-front-end.vercel.app/"
+//   );
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 app.get("/", function (req, res, next) {
   res.send("Runing");
